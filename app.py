e0f5146cd35d8942d498f54a292b475814c888f6
Nithyasri S <nithyasri0913@gmail.com>
2023-01-23 20:31:41 +0530

    app config

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from datetime import datetime
 #///- relative path
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
